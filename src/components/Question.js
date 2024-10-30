38dc762d3cb615c1b8d0d4521ae2086f89a872a5
Al Cerdan Lico <alcerdan@yahoo.com>
2024-10-30 13:24:06 -0700

    complete react hooks use effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  // useEffect(() => {
+  //   const interval = setTimeout(() => {
+  //     setTimeRemaining(() => {
+  //       if (timeRemaining === 0) {
+  //         onAnswered(false)
+  //         return 10
+  //       } else {
+  //         return timeRemaining - 1
+  //       }
+  //     })
+  //   }, 1000);
+  
+  //   return function cleanup() {
+  //     clearTimeout(interval)
+  //   }
+
+  // });
+
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      const interval = setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+
+      return () => clearTimeout(interval);
+    } else {
+      onAnswered(false);
+    }
+  }, [timeRemaining]);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
